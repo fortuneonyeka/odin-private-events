cf000237976f13a55c8f42c6db3f0bde010fb95f
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-13 09:44:25 +0100

    generate session controller, event controller,model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_081803) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_083848) do
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.string "location"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
