cedca13d4c246881bfde4eb21bf1e50b1a8b5c1d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-15 09:58:03 +0100

    add eventsControl create and show actions

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,30 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :description)
+  end
+end
