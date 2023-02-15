d8504b8899eb7d0fc2fd1ba8ffb900e9234be690
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-15 09:59:36 +0100

    event routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "event#index"
+  root "events#index"
+  resources :events
 end
