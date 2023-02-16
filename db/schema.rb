090ece46887f83062417ccd7964ce992b053e9ec
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-16 10:01:44 +0100

    generate attendance model, controller and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_083848) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_084255) do
+  create_table "attendances", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_attendances_on_event_id"
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.date "date"
@@ -27,4 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_083848) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "attendances", "events"
+  add_foreign_key "attendances", "users"
 end
