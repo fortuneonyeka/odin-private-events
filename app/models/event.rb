9b4ca7b3d207616b3f07171d05a53ffbd6ee9534
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-16 09:58:58 +0100

    update event-user-attendance association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creater, class_name: "User",foreign_key: "user_id"
+  has_many :attendance
+  has_many :attendees, through: :attendance, source: :user
 end
