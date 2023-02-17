2a28540b9ad1355a3245af336094573a6f71736d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-17 11:58:05 +0100

    handle action create user errors

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :require_login, except: [:new, :create]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash.notice = "User #{@user.name}, you are welcome and thank you for signing up!"
+      redirect_to users_path
+    else
+      flash[:alert] = "Please fix these #{@user.errors.count} errors! below"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+end
