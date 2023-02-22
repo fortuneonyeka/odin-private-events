fc5b71ba0197ad9e2018f7bc18292a0b54e79b5a
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-22 12:33:46 +0100

    views

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
 
   # get 'signup' => 'users#new', as: 'signup'
+  
   get 'login' => 'sessions#new', as: 'login'
   get 'logout' => 'sessions#destroy', as: 'logout'
 
